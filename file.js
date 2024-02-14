29fd57386a5a70f56785d7218981b818d70eac8e
Priyatarak1720 <139735312+Priyatarak1720@users.noreply.github.com>
2024-02-15 00:38:03 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,6 @@
+function myFun(){
+    document.getElementById("para1").style.color="red";
+    document.getElementById("para2").style.color="red";
+    document.getElementById("para3").style.color="red";
+    document.getElementById("list").style.color="red";
+}
